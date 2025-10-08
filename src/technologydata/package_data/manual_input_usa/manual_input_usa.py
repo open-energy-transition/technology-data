658a937556acf1bce8ea990c8a1c96f78ca53d31
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2025-10-08 14:45:43 +0200

    code: create parser file

---
+++ src/technologydata/package_data/manual_input_usa/manual_input_usa.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: The technology-data authors
+#
+# SPDX-License-Identifier: MIT
+
+"""Data parser for the manual_input_usa.csv data set."""
+import logging
+import pathlib
+
+path_cwd = pathlib.Path.cwd()
+
+logger = logging.getLogger(__name__)
